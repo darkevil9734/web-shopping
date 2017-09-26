6261a4789da13d709c63a410b75383dfb9f9d4cb
NGUYEN <darkevil9712@gmail.com>
2017-09-26 22:22:54 +0700

    Xay dung co so du lieu

---
+++ src/Services/Database.java
@@ -0,0 +1,19 @@
+package Services;
+
+import java.sql.*;
+
+public class Database {
+	public static Connection connect(){
+		Connection db = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/hthong_muaban?useUnicode=true&characterEncoding=UTF-8";
+			db = DriverManager.getConnection(url, "root", "");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return db;
+	}
+}
