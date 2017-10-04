c2fecfa414207c1e651972c3466d65f3f08e05d7
NGUYEN <darkevil9712@gmail.com>
2017-10-04 19:08:46 +0700

    các phương thức tìm kiếm theo giá tiền

--- src/BusinessLogics/sanPhamBL.java
+++ src/BusinessLogics/sanPhamBL.java
@@ -54,4 +54,129 @@ public class sanPhamBL {
 		return dsSanPham;
 		
 	}
+	
+	public static List<sanPham> timTheoGiaDuoi1Trieu(){
+		List<sanPham> dsSanPham = null;
+		try {
+			Connection db = Database.connect();
+			Statement stm = db.createStatement();
+			ResultSet rs = stm.executeQuery("select * from san_pham where gia_san_pham <1000000");
+			dsSanPham = new ArrayList<>();
+			while(rs.next()){
+				sanPham sp = new sanPham();
+				sp.setTenSanPham(rs.getString("ten_san_pham"));
+				sp.setGiaSanPham(rs.getInt("gia_san_pham"));
+				sp.setTinhTrang(rs.getString("tinh_trang"));
+				sp.setHinh_dai_dien(rs.getString("hinh_anh_mo_phong"));
+				dsSanPham.add(sp);
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return dsSanPham;
+		
+	}
+	
+	public static List<sanPham> timTheoGia1TrieuDen3Trieu(){
+		List<sanPham> dsSanPham = null;
+		try {
+			Connection db = Database.connect();
+			Statement stm = db.createStatement();
+			ResultSet rs = stm.executeQuery("select * from san_pham where gia_san_pham >=1000000 and gia_san_pham <3000000 ");
+			dsSanPham = new ArrayList<>();
+			while(rs.next()){
+				sanPham sp = new sanPham();
+				sp.setTenSanPham(rs.getString("ten_san_pham"));
+				sp.setGiaSanPham(rs.getInt("gia_san_pham"));
+				sp.setTinhTrang(rs.getString("tinh_trang"));
+				sp.setHinh_dai_dien(rs.getString("hinh_anh_mo_phong"));
+				dsSanPham.add(sp);
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return dsSanPham;
+		
+	}
+	
+	public static List<sanPham> timTheoGia3TrieuDen7Trieu(){
+		List<sanPham> dsSanPham = null;
+		try {
+			Connection db = Database.connect();
+			Statement stm = db.createStatement();
+			ResultSet rs = stm.executeQuery("select * from san_pham where gia_san_pham >=3000000 and gia_san_pham <7000000 ");
+			dsSanPham = new ArrayList<>();
+			while(rs.next()){
+				sanPham sp = new sanPham();
+				sp.setTenSanPham(rs.getString("ten_san_pham"));
+				sp.setGiaSanPham(rs.getInt("gia_san_pham"));
+				sp.setTinhTrang(rs.getString("tinh_trang"));
+				sp.setHinh_dai_dien(rs.getString("hinh_anh_mo_phong"));
+				dsSanPham.add(sp);
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return dsSanPham;
+		
+	}
+	
+	public static List<sanPham> timTheoGia7TrieuDen10Trieu(){
+		List<sanPham> dsSanPham = null;
+		try {
+			Connection db = Database.connect();
+			Statement stm = db.createStatement();
+			ResultSet rs = stm.executeQuery("select * from san_pham where gia_san_pham >=7000000 and gia_san_pham <10000000 ");
+			dsSanPham = new ArrayList<>();
+			while(rs.next()){
+				sanPham sp = new sanPham();
+				sp.setTenSanPham(rs.getString("ten_san_pham"));
+				sp.setGiaSanPham(rs.getInt("gia_san_pham"));
+				sp.setTinhTrang(rs.getString("tinh_trang"));
+				sp.setHinh_dai_dien(rs.getString("hinh_anh_mo_phong"));
+				dsSanPham.add(sp);
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return dsSanPham;
+		
+	}
+	
+	public static List<sanPham> timTheoGiaTren10Trieu(){
+		List<sanPham> dsSanPham = null;
+		try {
+			Connection db = Database.connect();
+			Statement stm = db.createStatement();
+			ResultSet rs = stm.executeQuery("select * from san_pham where gia_san_pham >=10000000");
+			dsSanPham = new ArrayList<>();
+			while(rs.next()){
+				sanPham sp = new sanPham();
+				sp.setTenSanPham(rs.getString("ten_san_pham"));
+				sp.setGiaSanPham(rs.getInt("gia_san_pham"));
+				sp.setTinhTrang(rs.getString("tinh_trang"));
+				sp.setHinh_dai_dien(rs.getString("hinh_anh_mo_phong"));
+				dsSanPham.add(sp);
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return dsSanPham;
+		
+	}
 }
