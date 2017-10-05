0b35498d3b3ddbeeb0429cb25a00e3a8ae45dfd7
NGUYEN <darkevil9712@gmail.com>
2017-10-05 20:07:24 +0700

    Trang lỗi khi không tìm thấy sản phẩm

--- src/Controller/timKiemSanPham.java
+++ src/Controller/timKiemSanPham.java
@@ -7,35 +7,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class timKiemSanPham
- */
+import BusinessLogics.sanPhamBL;
+
 @WebServlet("/timKiemSanPham")
 public class timKiemSanPham extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public timKiemSanPham() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
+  
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		
 	}
 
 }
