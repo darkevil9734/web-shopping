4cc63cff45ffe0f199a3d9243694bb95b577fbdc
NGUYEN <darkevil9712@gmail.com>
2017-10-09 10:26:56 +0700

    Cập nhật hàm gửi mail góp ý về admin trang web

--- src/Controller/GopYServlet.java
+++ src/Controller/GopYServlet.java
@@ -1,40 +1,86 @@
 package Controller;
 
 import java.io.IOException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class GopYServlet
- */
 @WebServlet("/GopYServlet")
 public class GopYServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public GopYServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String comment = request.getParameter("message");
+		
+		if(email != null) {
+			final String username = "testmailbaitap@gmail.com";
+			final String password = "Vuhoangnguyen";
+
+			Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
+			
+			Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+			});
+			
+			try {
+
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(email));
+				message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse("testmailbaitap@gmail.com"));
+				message.setSubject("Góp ý");
+				message.setText("Chào admin,"
+					+ "\n\n Tôi tên là: " + name
+					+ "\n\n Góp ý: " + comment
+					+ "\n\n Mong admin kiểm tra góp ý của tôi"
+					+ "\n\n Thân "
+					+ "\n\n" + name);
+
+				Transport.send(message);
+
+				System.out.println("Done");
+				request.getRequestDispatcher("Views/Success.jsp").forward(request, response);
+			} catch (MessagingException e) {
+				request.getRequestDispatcher("Views/loi.jsp").forward(request, response);
+
+				throw new RuntimeException(e);
+			}
+		}
+		else {
+			request.getRequestDispatcher("Views/loi.jsp").forward(request, response);
+		}
+		
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
