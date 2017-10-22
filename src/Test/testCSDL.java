c1245ccda9d5f31f68baa31bf2faa25190164ed2
NGUYEN <darkevil9712@gmail.com>
2017-10-22 20:09:50 +0700

    Unit Test CSDL

--- src/Test/testCSDL.java
+++ src/Test/testCSDL.java
@@ -11,6 +11,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import BusinessLogics.Database;
+import BusinessLogics.quanLyGioHang;
+import BusinessLogics.sanPhamBL;
 import BusinessLogics.userBL;
 
 public class testCSDL {
@@ -29,7 +31,26 @@ public class testCSDL {
 	
 	@Test
 	public void findAllUser() {
+		// tổng cộng hiện tại là 3
 		assertEquals(3 , userBL.docTatCa().size());
 	}
-
+	
+	@Test
+	public void findAllSanPhamFail() {
+		// tổng cộng hiện tại là 45
+		assertEquals(3 , sanPhamBL.DocTatCa().size());
+	}
+	
+	@Test
+	public void findAllSanPham() {
+		// tổng cộng hiện tại là 45
+		assertEquals(45 , sanPhamBL.DocTatCa().size());
+	}
+	
+	@Test
+	public void findAllGioHang() {
+		assertEquals(1 , quanLyGioHang.xemTatCa().size());
+	}
+	
+	
 }
