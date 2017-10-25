3a7c6af45eaf28eda22b20bd2b2a54f67c2fbb89
NGUYEN <darkevil9712@gmail.com>
2017-10-25 15:29:58 +0700

    CSharp WinForm Test Selenium

---
+++ Selenium C# Test/SeleniumCSharpTest/SeleniumTest/formTestChinh.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeleniumTest
+{
+    public partial class formTestChinh : Form
+    {
+        public formTestChinh()
+        {
+            InitializeComponent();
+        }
+
+        private void formTestChinh_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
