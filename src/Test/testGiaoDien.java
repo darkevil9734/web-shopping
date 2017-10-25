1ce3550fe4e3bd080c308c8a6091904f6c48163a
NGUYEN <darkevil9712@gmail.com>
2017-10-25 13:09:48 +0700

    selenium test

---
+++ src/Test/testGiaoDien.java
@@ -0,0 +1,45 @@
+package Test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+
+@Test
+public class testGiaoDien {
+	
+	@Test
+	public void moTrangChu() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "F:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
+		try {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--disable-notifications");
+			WebDriver driver = new ChromeDriver(options);
+			driver.get("http://localhost:8080/web-mobile/trang-chu.jsp");
+			//driver.manage().window().maximize();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}	
+	}
+	
+	@Test
+	public void moTrangDangNhap() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "F:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
+		try {
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--disable-notifications");
+			WebDriver driver = new ChromeDriver(options);
+			driver.get("http://localhost:8080/web-mobile/dang-nhap.jsp");
+			//driver.manage().window().maximize();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}	
+	}
+	
+	
+	
+}
