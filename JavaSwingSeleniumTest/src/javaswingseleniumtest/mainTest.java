41334750927d9e79dbe6b8615857fe9e3b47d02a
NGUYEN <darkevil9712@gmail.com>
2017-11-06 23:05:33 +0700

    config Server Glassfish ở local để đồng nhất port với nhất giữa các members

--- JavaSwingSeleniumTest/src/javaswingseleniumtest/mainTest.java
+++ JavaSwingSeleniumTest/src/javaswingseleniumtest/mainTest.java
@@ -34,24 +34,57 @@ public class mainTest extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         btnMoTrangChu = new javax.swing.JButton();
         btnMoTrangDangNhap = new javax.swing.JButton();
+        btnMoTrangDangKy = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Test Selenium Swing");
 
         jLabel1.setIcon(new javax.swing.ImageIcon("G:\\test.png")); // NOI18N
 
-        btnMoTrangChu.setText("Mở trang chủ");
+        btnMoTrangChu.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
+        btnMoTrangChu.setText("Test Trang Chủ");
         btnMoTrangChu.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnMoTrangChuActionPerformed(evt);
             }
         });
 
-        btnMoTrangDangNhap.setText("Mở trang đăng nhập");
+        btnMoTrangDangNhap.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
+        btnMoTrangDangNhap.setText("Test Trang Đăng Nhập");
         btnMoTrangDangNhap.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnMoTrangDangNhapActionPerformed(evt);
             }
         });
+
+        btnMoTrangDangKy.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
+        btnMoTrangDangKy.setText("Test Trang Đăng Ký");
+        btnMoTrangDangKy.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMoTrangDangKyActionPerformed(evt);
+            }
+        });
+
+        jMenu1.setText("Menu");
+
+        jMenuItem1.setText("Thoát");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -64,9 +97,11 @@ public class mainTest extends javax.swing.JFrame {
                         .addComponent(jLabel1))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(69, 69, 69)
-                        .addComponent(btnMoTrangChu, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(77, 77, 77)
-                        .addComponent(btnMoTrangDangNhap, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btnMoTrangChu, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(55, 55, 55)
+                        .addComponent(btnMoTrangDangNhap)
+                        .addGap(55, 55, 55)
+                        .addComponent(btnMoTrangDangKy, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(103, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -77,31 +112,31 @@ public class mainTest extends javax.swing.JFrame {
                 .addGap(88, 88, 88)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnMoTrangChu, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnMoTrangDangNhap, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(106, Short.MAX_VALUE))
+                    .addComponent(btnMoTrangDangNhap, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnMoTrangDangKy, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(80, Short.MAX_VALUE))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnMoTrangChuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMoTrangChuActionPerformed
-        // TODO add your handling code here:
-        testGiaoDien testHomePage = new testGiaoDien();
-        try {
-            testHomePage.moTrangChu();
-        } catch (Exception ex) {
-            Logger.getLogger(mainTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        TrangChu home = new TrangChu();
+        home.setVisible(true);
     }//GEN-LAST:event_btnMoTrangChuActionPerformed
 
     private void btnMoTrangDangNhapActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMoTrangDangNhapActionPerformed
-        testGiaoDien testLogin = new testGiaoDien();
-        try {
-            testLogin.moTrangDangNhap();
-        } catch (Exception ex) {
-            Logger.getLogger(mainTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
     }//GEN-LAST:event_btnMoTrangDangNhapActionPerformed
+
+    private void btnMoTrangDangKyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMoTrangDangKyActionPerformed
+                
+    }//GEN-LAST:event_btnMoTrangDangKyActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -140,7 +175,12 @@ public class mainTest extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnMoTrangChu;
+    private javax.swing.JButton btnMoTrangDangKy;
     private javax.swing.JButton btnMoTrangDangNhap;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     // End of variables declaration//GEN-END:variables
 }
